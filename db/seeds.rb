a6bc053d28b5c6c49c8a6cd74828e2fd65ddf4dc
Danila Makarov <danila.makarov@gmail.com>
2019-04-01 17:58:46 +0300

    First functional block. Displays projects and tasks.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "active_support/core_ext/hash/indifferent_access"
+require "yaml"
+seeds = HashWithIndifferentAccess.new(YAML.load_file(File.join(__dir__, 'seeds/seeds.yml') ))
+seeds['projects'].each do |proj|
+    @project = Project.create(title: proj['title'])
+    proj['todos'].each do |todo|
+        Todo.create(text: todo['text'], isCompleted: todo['isCompleted'], project: @project)
+    end
+end
