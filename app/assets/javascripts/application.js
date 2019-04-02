4898ca07e9a7ea9f7fbd854cd51863a3533baa80
Danila Makarov <danila.makarov@gmail.com>
2019-04-02 15:53:29 +0300

    functional block 1.5

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,22 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require rails-ujs
+//= require jquery3
+//= require jquery_ujs
 //= require activestorage
 //= require_tree .
+
+$(document).on("click", "#submit_link", function(){
+   $("#form_id").submit();
+});
+
+$(document).on("click", "#cancell_link", function(){
+   $("#mdiv").hide();
+});
+
+$(document).ready(function() {
+    $("#mdiv").hide();
+    $("#new_btn").click(function(){
+        $("#mdiv").show();
+    });
+});
--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -5,5 +5,12 @@ class ProjectsController < ApplicationController
     def update
     end
     def create
+        @todo = Todo.new(text: todo_params['text'], isCompleted: false, project: Project.find(todo_params['project_id'].to_i))
+        @todo.save
+        redirect_to root_url
+    end
+
+    private def todo_params
+        params.permit(:text, :project_id)
     end
 end
