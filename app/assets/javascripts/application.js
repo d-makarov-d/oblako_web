d8c3e1062298ef7b8f80f8b46742d7f41a40d18e
Danila Makarov <danila.makarov@gmail.com>
2019-04-06 00:31:26 +0300

    JS, CSS, bootstrap, updating tascks isCompleted

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,8 @@
 // about supported directives.
 //
 //= require jquery
+//= require icheck
+//= require select2
 //= reuire_ujs
 //= require activestorage
 //= require_tree .
@@ -22,13 +24,51 @@ $(document).on("click", "#submit_link", function(){
 
 $(document).on("click", "#cancell_link", function(){
     event.preventDefault();
-   $("#form_id").hide();
+   $(".glob").hide();
 });
 
 $(document).ready(function() {
-    $("#form_id").hide();
+    $(".glob").hide();
     $("#new_btn").click(function(){
         event.preventDefault();
-        $("#form_id").show();
+        $(".glob").show();
+    });
+    $('select#project_id').select2({
+        minimumResultsForSearch: -1
     });
 });
+
+function icheck(){
+  if($(".icheck-me").length > 0){
+    $(".icheck-me").each(function(){
+      var $el = $(this);
+      var skin = ($el.attr('data-skin') !== undefined) ? "_" + $el.attr('data-skin') : "",
+      color = ($el.attr('data-color') !== undefined) ? "-" + $el.attr('data-color') : "";
+      var opt = {
+        checkboxClass: 'icheckbox' + skin + color,
+        radioClass: 'iradio' + skin + color,
+      }
+      $el.iCheck(opt);
+      var li_id = ($el.attr('li_id') !== undefined) ? $el.attr('li_id') : "";
+      $("#todo" + li_id).on('click', function(){
+          $("#todo" + li_id).submit();
+      })
+      $el.on('ifChecked', function(event){
+          $("#" + li_id).css({
+              "text-decoration": "line-through"
+          })
+          $("#todo" + li_id).submit();
+      });
+      $el.on('ifUnchecked', function(event){
+          $("#" + li_id).css({
+              "text-decoration": "none"
+          })
+          $("#todo" + li_id).submit();
+      });
+    });
+  }
+}
+
+$(function(){
+  icheck();
+})
--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -3,6 +3,17 @@ class ProjectsController < ApplicationController
         @projects = Project.all
     end
     def update
+        @todo = Todo.find(iscompleted_params['todo_id'].to_i)
+        puts @todo.isCompleted
+        puts 'changed'
+        if @todo.isCompleted then
+            @todo.isCompleted = false
+        else
+            @todo.isCompleted = true
+        end
+        puts @todo.isCompleted
+        @todo.save
+        redirect_to root_url
     end
     def create
         @todo = Todo.new(text: todo_params['text'], isCompleted: false, project: Project.find(todo_params['project_id'].to_i))
@@ -12,5 +23,8 @@ class ProjectsController < ApplicationController
 
     private def todo_params
         params.permit(:text, :project_id)
+    end
+    private def iscompleted_params
+        params.permit(:todo_id)
     end
 end
